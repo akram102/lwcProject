0e5fac68243349e2cc9124806813825b734381aa
akram102 <akram102zaki@gmail.com>
2022-06-10 17:22:19 +0530

    Adding new component flightRecordCreation

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,10 +1,17 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import {ShowToastEvent} from '@salesforce/platformShowToastEvent';
 
 
 // imports
 // import BOAT_REVIEW_OBJECT from schema - BoatReview__c
+import BOAT_REVIEW_OBJECT from '@salesforce/schema/BoatReview__c';
 // import NAME_FIELD from schema - BoatReview__c.Name
+import NAME_FIELD from '@salesforce/schema/BoatReview__c.Name';
 // import COMMENT_FIELD from schema - BoatReview__c.Comment__c
+import COMMENT_FIELD from '@salesforce/schema/BoatReview__c.Comment__c';
+
+const SUCCESS_TITLE = 'Review Created!';
+const SUCCESS_VARIANT = 'success';
 export default class BoatAddReviewForm extends LightningElement {
     // Private
     boatId;
@@ -16,10 +23,15 @@ export default class BoatAddReviewForm extends LightningElement {
     labelRating  = 'Rating';
     
     // Public Getter and Setter to allow for logic to run on recordId change
-    get recordId() { }
+    @api
+    get recordId() { 
+        return this.boatId
+    }
     set recordId(value) {
       //sets boatId attribute
+      this.setAttribute('boatId',this.boatId)
       //sets boatId assignment
+      this.boatId = value;
     }
     
     // Gets user rating input from stars component
@@ -34,6 +46,11 @@ export default class BoatAddReviewForm extends LightningElement {
     // Dispatches event when a review is created
     handleSuccess() {
       // TODO: dispatch the custom event and show the success message
+      const toast = new ShowToastEvent({
+          title : SUCCESS_TITLE,
+          variant : SUCCESS_VARIANT
+      });
+      this.dispatchEvent(toast);
       this.handleReset();
     }
     
---
+++ force-app/main/default/lwc/flightRecordCreation/flightRecordCreation.js
@@ -0,0 +1,107 @@
+import { LightningElement, wire } from 'lwc';
+import getAirports from '@salesforce/apex/FlightController.getAirports';
+import calculateDistance from '@salesforce/apex/FlightController.calculateDistance';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import FLIGHT_OBJECT from '@salesforce/schema/Flight__c';
+const ERROR_TITLE = 'ERROR'
+const ERROR_VARIANT = 'error';
+
+export default class FlightRecordCreation extends NavigationMixin(LightningElement) {
+
+    wiredAirportData
+    distanceCalculated
+    options
+    selectedDepartureFlight
+    departureFlight
+    selectedArrivalFlight
+    arrivalFlight
+    departureLatitude
+    departureLongitude
+    arrivalLatitude
+    arrivalLongitude
+
+
+    @wire(getAirports)
+    wiredData({data,error}){
+        if(data){
+            this.wiredAirportData = data;
+            this.options = data.map((airport)=>{
+                return {
+                    label: airport.IATA__c, value: airport.Id
+                }
+            })
+        }
+        else if(error){
+            console.log('error---->',error);
+        }
+    }
+
+    handleDepartureFlightChange(event){
+        this.selectedDepartureFlight = event.detail.value;
+        let deptLabel = this.options.find((key)=>{
+            return key.value === event.detail.value
+        });
+        this.departureFlight = deptLabel.label;
+        this.wiredAirportData.find((airport)=>{
+            if(airport.Id === this.selectedDepartureFlight){
+                this.departureLatitude = airport.Location__Latitude__s;
+                this.departureLongitude = airport.Location__Longitude__s;
+            }
+        })
+    }
+    handleArrivaleFlightChange(event){
+        this.selectedArrivalFlight = event.detail.value;
+        let arrLabel = this.options.find((key)=>{
+            return key.value === event.detail.value
+        });
+        this.arrivalFlight = arrLabel.label
+        this.wiredAirportData.find((airport)=>{
+            if(airport.Id === this.selectedArrivalFlight){
+                this.arrivalLatitude = airport.Location__Latitude__s;
+                this.arrivalLongitude = airport.Location__Longitude__s;
+            }
+        })
+    }
+    handleSuccess(event){
+        let newRecordCreated = event.detail.id;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: newRecordCreated,
+                objectApiName: FLIGHT_OBJECT,
+                actionName: 'view'
+            }
+        });
+    }
+    async handleSubmit(event){
+        await calculateDistance({latitude1 : this.departureLatitude, longitude1:this.departureLongitude, latitude2 : this.arrivalLatitude,longitude2 : this.arrivalLongitude})
+        .then((result)=>{
+            this.distanceCalculated = result;
+        }).catch((error)=>{
+            console.log('error in dis---->',error);
+        })
+        if(this.departureFlight === this.arrivalFlight){
+            throw new Error("Departure and Arrival airports can't be same")
+        }
+        event.preventDefault();
+        const fields = event.detail.fields;
+        console.log('field--->',fields)
+        fields.Distance__c = this.distanceCalculated;
+        fields.Departure__c = this.departureFlight;
+        fields.Arrival__c = this.arrivalFlight;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+        
+    }
+    handleError(event){
+        if(this.departureFlight === this.arrivalFlight){
+            const toast = new ShowToastEvent({
+                title : ERROR_TITLE,
+                message : event.detail.detail,
+                variant : ERROR_VARIANT
+            })
+            this.dispatchEvent(toast);
+        }   
+    }   
+}
